db27379fb586c4cef0f575c3c76b7de7aadeff11
H-eitordias <heitordias2706@gmail.com>
2025-06-20 23:52:38 -0300

    Create ex15.go

---
+++ lista01/ex15.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	if n >= 2 {
+		if n >= 2 {
+			fmt.Printf("2^2 = 4\n")
+		}
+		if n >= 4 {
+			fmt.Printf("4^2 = 16\n")
+		}
+		if n >= 6 {
+			fmt.Printf("6^2 = 36\n")
+		}
+		if n >= 8 {
+			fmt.Printf("8^2 = 64\n")
+		}
+		if n >= 10 {
+			fmt.Printf("10^2 = 100\n")
+		}
+
+	}
+}
