2bfd4896dc556db301eeae7ee3841c5e4eec8a2f
H-eitordias <heitordias2706@gmail.com>
2025-06-21 01:36:39 -0300

    Update ex10.go

--- lista02/ex10.go
+++ lista02/ex10.go
@@ -8,48 +8,39 @@ func main() {
 	var destino, tipo int
 	var preco float64
 
-	fmt.Println("1 - Região Norte")
-	fmt.Println("2 - Região Nordeste")
-	fmt.Println("3 - Região Centro-Oeste")
-	fmt.Println("4 - Região Sul")
-	fmt.Print("Digite o destino (1 a 4): ")
+	fmt.Print("Destino (1-Norte, 2-Nordeste, 3-Centro-Oeste, 4-Sul): ")
 	fmt.Scanln(&destino)
 
-	fmt.Println("1 - Ida e volta")
-	fmt.Println("2 - Só ida")
-	fmt.Print("Inclui retorno? (1 para sim, 2 para não): ")
+	fmt.Print("Tipo (1-Ida e Volta, 2-Só Ida): ")
 	fmt.Scanln(&tipo)
 
-	if destino < 1 || destino > 4 || tipo < 1 || tipo > 2 {
-		fmt.Println("Opção inválida.")
-		return
-	}
-
-	switch destino {
-	case 1:
+	if destino == 1 {
 		if tipo == 1 {
 			preco = 900
 		} else {
 			preco = 500
 		}
-	case 2:
+	} else if destino == 2 {
 		if tipo == 1 {
 			preco = 650
 		} else {
 			preco = 350
 		}
-	case 3:
+	} else if destino == 3 {
 		if tipo == 1 {
 			preco = 600
 		} else {
 			preco = 350
 		}
-	case 4:
+	} else if destino == 4 {
 		if tipo == 1 {
 			preco = 550
 		} else {
 			preco = 300
 		}
+	} else {
+		fmt.Println("Destino inválido.")
+		return
 	}
 
 	fmt.Printf("Preço da passagem: R$ %.2f\n", preco)
